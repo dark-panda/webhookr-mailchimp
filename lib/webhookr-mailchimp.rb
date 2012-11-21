f89d30587812de9dba84b468f6b6c7e097d010ae
Gerry Power <gerry@zoocasa.com>
2012-11-20 21:53:53 -0500

    Refactored to SRP

--- lib/webhookr-mailchimp.rb
+++ lib/webhookr-mailchimp.rb
@@ -1,39 +1,44 @@
-require "webhookr-mailchimp/version"
 require "webhookr"
 require "active_support/core_ext/module/attribute_accessors"
 require "recursive_open_struct"
-require "webhookr/invalid_payload_error"
 
 module Webhookr
   module Services
     module Mailchimp
+      extend ActiveSupport::Autoload
+
+      autoload :VERSION
+
       mattr_accessor :config
       self.config = ActiveSupport::OrderedOptions.new
 
       class Adapter
         EVENT_KEY = "type"
-        EVENT_DATE_KEY = "fired_at"
         PAYLOAD_KEY = "data"
 
-        def process(raw_response)
-          p = Rack::Utils.parse_nested_query(raw_response)
-          assert_valid_packet(p)
+        def self.process(raw_response)
+          new.process(raw_response)
+        end
 
-          return [ OpenStruct.new(
-                      :event_type => p[EVENT_KEY],
-                      :payload => RecursiveOpenStruct.new(p, :recurse_over_arrays => true)
-                    )
-                 ]
+        def process(raw_response)
+          p = parse(raw_response)
+          return [ Webhookr::AdapterResponse.new(p[EVENT_KEY], RecursiveOpenStruct.new(p)) ]
         end
 
         private
 
+        def parse(raw_response)
+          assert_valid_packet(Rack::Utils.parse_nested_query(raw_response))
+        end
+
         def assert_valid_packet(parsed_response)
           raise(Webhookr::InvalidPayloadError, "Missing event key '#{EVENT_KEY}' in packet") unless parsed_response[EVENT_KEY].present?
           raise(Webhookr::InvalidPayloadError, "No data key '#{PAYLOAD_KEY}' in the response") unless parsed_response[PAYLOAD_KEY].present?
+          parsed_response
         end
 
       end
+
     end
   end
 end
