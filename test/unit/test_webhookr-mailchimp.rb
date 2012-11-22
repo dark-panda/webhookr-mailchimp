a0fec60872b87b20b9117aaf5b104644829893a1
Gerry Power <gerry@zoocasa.com>
2012-11-21 19:54:27 -0500

    White space changes

--- test/unit/test_webhookr-mailchimp.rb
+++ test/unit/test_webhookr-mailchimp.rb
@@ -30,49 +30,49 @@ describe Webhookr::Mailchimp::Adapter do
 
   subject { Webhookr::Mailchimp::Adapter }
 
-  it "must support process" do
-    subject.must_respond_to(:process)
-  end
+    it "must support process" do
+      subject.must_respond_to(:process)
+    end
 
   end
 
   describe "the instance" do
 
-  subject { Webhookr::Mailchimp::Adapter.new }
+    subject { Webhookr::Mailchimp::Adapter.new }
 
-  it "should not return an error for a valid packet" do
-    lambda {
-      subject.process(@valid_response)
-    }.must_be_silent
-  end
+    it "should not return an error for a valid packet" do
+      lambda {
+        subject.process(@valid_response)
+      }.must_be_silent
+    end
 
-  it "should return the correct event type" do
-    response = subject.process(@valid_response).first
-    assert_equal(@event_type, response.event_type)
-  end
+    it "should return the correct event type" do
+      response = subject.process(@valid_response).first
+      assert_equal(@event_type, response.event_type)
+    end
 
-  it "should return the correct data packet" do
-    response = subject.process(@valid_response).first
-    assert_equal("gerry+agent2@zoocasa.com", response.payload.data.email)
-  end
+    it "should return the correct data packet" do
+      response = subject.process(@valid_response).first
+      assert_equal("gerry+agent2@zoocasa.com", response.payload.data.email)
+    end
 
-  it "should raise MailObserver::InvalidPayloadError for no packet" do
-    lambda {
-      subject.process("")
-    }.must_raise(Webhookr::InvalidPayloadError)
-  end
+    it "should raise MailObserver::InvalidPayloadError for no packet" do
+      lambda {
+        subject.process("")
+      }.must_raise(Webhookr::InvalidPayloadError)
+    end
 
-  it "should raise MailObserver::InvalidPayloadError for a missing event type" do
-    lambda {
-      subject.process("data[email]=gerry%2Bagent2@zoocasa.com")
-    }.must_raise(Webhookr::InvalidPayloadError)
-  end
+    it "should raise MailObserver::InvalidPayloadError for a missing event type" do
+      lambda {
+        subject.process("data[email]=gerry%2Bagent2@zoocasa.com")
+      }.must_raise(Webhookr::InvalidPayloadError)
+    end
 
-  it "should raise MailObserver::InvalidPayloadError for a missing data packet" do
-    lambda {
-      subject.process("type=unsubscribe")
-    }.must_raise(Webhookr::InvalidPayloadError)
-  end
+    it "should raise MailObserver::InvalidPayloadError for a missing data packet" do
+      lambda {
+        subject.process("type=unsubscribe")
+      }.must_raise(Webhookr::InvalidPayloadError)
+    end
 
   end
 
