62a10cf5a64a93a8a5b7b0c6971bc065f45c22f1
J Smith <dark.panda@gmail.com>
2013-07-29 14:55:39 -0400

    Add version information to test output.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,9 @@ require File.expand_path('../../lib/webhookr-mailchimp.rb', __FILE__)
 if RUBY_VERSION >= "1.9"
   MiniTest::Reporters.use!(MiniTest::Reporters::SpecReporter.new)
 end
+
+puts "Webhookr #{Webhookr::VERSION}"
+puts "Webhookr::Mailchimp #{Webhookr::Mailchimp::VERSION}"
+puts "Rails #{Rails::VERSION::STRING}"
+puts "Ruby #{RUBY_VERSION}-p#{RUBY_PATCHLEVEL} - #{RbConfig::CONFIG['RUBY_INSTALL_NAME']}"
+
